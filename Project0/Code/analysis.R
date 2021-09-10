5af1a109e6c19cb3e0400c63e6b2c1575d8428a0
Max McGrath <max.a.mcgrath@gmail.com>
2021-09-10 08:00:26 -0600

    Reorganize code
    Add analysis.R

---
+++ Project0/Code/analysis.R
@@ -0,0 +1,41 @@
+# Aim 1 ------------------------------------------------------------------------
+# Read data
+withDateTimes24 <- read_rds("DataProcessed/withDateTimes24")
+
+# Plot Data
+ggplot(data = withDateTimes24, mapping = aes(x = bookletTime, y = memTime)) +
+    geom_point() +
+    geom_smooth(method = 'lm')
+
+
+# Aim 2 ------------------------------------------------------------------------
+# Read data
+questionTwoData <- read_rds("DataProcessed/questionTwoData")
+
+# Plot book versus MEM adherence
+ggplot(data = questionTwoData, aes(x = recordType, y = adherence)) +
+    geom_boxplot()
+
+# Create table comparing Book and MEM Adherence
+adherenceTable <- questionTwoData %>%
+    group_by(recordType) %>%
+    summarise(total = length(.data$adherence),
+              naCount = sum(is.na(.data$adherence)),
+              countLess7_5 = sum(abs(.data$adherence) <= 7.5, na.rm = TRUE),
+              percentLess7_5 = sum(abs(.data$adherence) <= 7.5, na.rm = TRUE) / 
+                  sum(!is.na(.data$adherence)), 
+              count7_5to15 = sum(abs(.data$adherence) > 7.5 & 
+                                     abs(.data$adherence) <= 15, na.rm = TRUE),
+              percent7_5to15 = sum(abs(.data$adherence) > 7.5 & 
+                                       abs(.data$adherence) <= 15, na.rm = TRUE) / 
+                  sum(!is.na(.data$adherence)),
+              countGreater15 = sum(abs(.data$adherence) > 15, na.rm = TRUE),
+              percentGreater15 = sum(abs(.data$adherence) > 15, na.rm = TRUE) / 
+                  sum(!is.na(.data$adherence)),
+              countLess15 = sum(abs(.data$adherence) <= 15, na.rm = TRUE),
+              percentLess15 = sum(abs(.data$adherence) <= 15, na.rm = TRUE) / 
+                  sum(!is.na(.data$adherence)))
+
+# Aim 3 ------------------------------------------------------------------------
+
+# Miscellaneous  ---------------------------------------------------------------
--- Project0/Code/dataPrep.R
+++ Project0/Code/dataPrep.R
@@ -38,12 +38,7 @@ withDateTimes24 <- dataRaw %>%
                format = "%H:%M")),
            diffMemBooklet = difftime(memTime, bookletTime, units = "mins"))
 
-cor(as.numeric(withDateTimes24$bookletTime), as.numeric(withDateTimes24$memTime),
-    use = "complete.obs")
-
-ggplot(data = withDateTimes24, mapping = aes(x = bookletTime, y = memTime)) +
-    geom_point() +
-    geom_smooth(method = 'lm')
+write_rds(withDateTimes24, "DataProcessed/withDateTimes24")
 
 # Limit data to 2nd (30 minutes after waking) and 4th (10 hours after waking) 
 #   observation, calculate respective adherence to study procedure, remove
@@ -60,53 +55,7 @@ questionTwoData <- withDateTimes %>%
                       .data$MEMs..Sample.interval.Decimal.Time..mins. - 600)) %>% 
     pivot_longer(cols = book:mem, names_to = "recordType", values_to = "adherence")
 
-# Plot book versus MEM adherence
-ggplot(data = questionTwoData, aes(x = recordType, y = adherence)) +
-    geom_boxplot()
+write_rds(questionTwoData, "DataProcessed/questionTwoData")
 
-# Create table comparing Book and MEM Adherence
-adherenceTable <- questionTwoData %>%
-    group_by(recordType) %>%
-    summarise(naCount = sum(is.na(.data$adherence)),
-              percentLess7_5 = sum(abs(.data$adherence) <= 7.5, na.rm = TRUE) / 
-                  sum(!is.na(.data$adherence)), 
-              percent7_5to15 = sum(abs(.data$adherence) > 7.5 & 
-                                       abs(.data$adherence) <= 15, na.rm = TRUE) / 
-                  sum(!is.na(.data$adherence)),
-              percentGreater15 = sum(abs(.data$adherence) > 15, na.rm = TRUE) / 
-                  sum(!is.na(.data$adherence)),
-              percentLess15 = sum(abs(.data$adherence) <= 15, na.rm = TRUE) / 
-                  sum(!is.na(.data$adherence)))
 
-# Plot diff vs bookletTime for EDA
-ggplot(data = withDateTimes, aes(x = bookletTime, y = diffMemBooklet, 
-                                 color = Collection.Sample)) +
-    geom_point() +
-    geom_hline(yintercept = 7.5, color = "red", alpha = .3) +
-    geom_hline(yintercept = -7.5, color = "red", alpha = .3) +
-    geom_hline(yintercept = 15, color = "red", alpha = .3) +
-    geom_hline(yintercept = -15, color = "red", alpha = .3)
-
-# Create table of classes for difference between bookletTime and memTime
-diffTimeTable <- withDateTimes %>%
-    drop_na(diffMemBooklet) %>%
-    summarize(percentLess7_5 = sum(abs(.data$diffMemBooklet) < 7.5) / 
-                  length(.data$diffMemBooklet), 
-              percent7_5to15 = sum(abs(.data$diffMemBooklet) > 7.5 & 
-                                       abs(.data$diffMemBooklet) < 15) / 
-                  length(.data$diffMemBooklet),
-              percentGreater15 = sum(abs(.data$diffMemBooklet) > 15) / 
-                  length(.data$diffMemBooklet))
-
-# Same as above but grouped by collection sample
-diffTimeTableGrouped <- withDateTimes %>%
-    drop_na(diffMemBooklet) %>%
-    group_by(Collection.Sample) %>%
-    summarize(percentLess7_5 = sum(abs(.data$diffMemBooklet) <= 7.5) / 
-                  length(.data$diffMemBooklet), 
-              percent7_5to15 = sum(abs(.data$diffMemBooklet) > 7.5 & 
-                                       abs(.data$diffMemBooklet) < 15) / 
-                  length(.data$diffMemBooklet),
-              percentGreater15 = sum(abs(.data$diffMemBooklet) >= 15) / 
-                  length(.data$diffMemBooklet))
 
