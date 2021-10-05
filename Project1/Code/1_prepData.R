5ac18eb9971b5e58e93b02b1cb0d0c0e863c6d14
Max McGrath <max.a.mcgrath@gmail.com>
2021-10-05 11:57:06 -0600

    Add data prep

--- Project1/Code/1_prepData.R
+++ Project1/Code/1_prepData.R
@@ -4,10 +4,29 @@ library(magrittr)
 
 dataRaw <- read.csv("DataRaw/hiv_6624_final.csv")
 
+# age, body mass index,smoking status, education, race/ethnicity, and adherence
+
+# Select baseline outcomes and relevant baseline covariates
 yearZero <- dataRaw %>%
     filter(years == 0) %>%
-    select(newid, VLOAD, LEU3N, AGG_MENT, AGG_PHYS, hard_drugs)
+    select(newid, VLOAD_0 = VLOAD, LEU3N_0 = LEU3N, AGG_MENT_0 = AGG_MENT, 
+           AGG_PHYS_0 = AGG_PHYS, hard_drugs_0 = hard_drugs, EDUC_0 = EDUCBAS,
+           age_0 = age, RACE_0 = RACE, BMI_0 = BMI, SMOKE_0 = SMOKE)
 
+# Select year two outcomes and relevant year two covariates
 yearTwo <- dataRaw %>%
     filter(years == 2) %>%
-    select(newid, VLOAD, LEU3N, AGG_MENT, AGG_PHYS, hard_drugs)
+    select(newid, VLOAD_2 = VLOAD, LEU3N_2 = LEU3N, AGG_MENT_2 = AGG_MENT, 
+           AGG_PHYS_2 = AGG_PHYS,
+           ADH_2 = ADH)
+
+# Combine data, create outcome difference columns
+fullData <- full_join(yearZero, yearTwo, by = "newid") %>%
+    mutate(VLOAD_DIFF = VLOAD_2 - VLOAD_0,
+           LEU3N_DIFF = LEU3N_2 - LEU3N_0,
+           MENT_DIFF = AGG_MENT_2 - AGG_MENT_0,
+           PHYS_DIFF = AGG_PHYS_2 - AGG_PHYS_0,
+           .keep = "unused")
+
+# Split education into subcategories
+# Will do later
