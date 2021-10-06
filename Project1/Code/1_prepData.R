632d86ffc8c205c71e337e4c5f6bfe0946b472a2
Max McGrath <max.a.mcgrath@gmail.com>
2021-10-06 17:28:30 -0600

    Reduce categories, seperate data by outcome
    Reduce race, smoking, and education categories into
    fewer categories. Seperate data by outcome in
    order to shorten equation arguments later down the
    line

--- Project1/Code/1_prepData.R
+++ Project1/Code/1_prepData.R
@@ -10,8 +10,8 @@ dataRaw <- read.csv("DataRaw/hiv_6624_final.csv")
 yearZero <- dataRaw %>%
     filter(years == 0) %>%
     select(NEWID = newid, VLOAD_0 = VLOAD, LEU3N_0 = LEU3N, 
-           AGG_MENT_0 = AGG_MENT, 
-           AGG_PHYS_0 = AGG_PHYS, DRUGS_0 = hard_drugs, EDUC_0 = EDUCBAS,
+           MENT_0 = AGG_MENT, 
+           PHYS_0 = AGG_PHYS, DRUGS_0 = hard_drugs, EDUC_0 = EDUCBAS,
            AGE_0 = age, RACE_0 = RACE, BMI_0 = BMI, SMOKE_0 = SMOKE) %>%
     mutate(LOG_VLOAD_0 = log10(VLOAD_0),
            .keep = "unused")
@@ -20,19 +20,51 @@ yearZero <- dataRaw %>%
 yearTwo <- dataRaw %>%
     filter(years == 2) %>%
     select(NEWID = newid, VLOAD_2 = VLOAD, LEU3N_2 = LEU3N, 
-           AGG_MENT_2 = AGG_MENT, 
-           AGG_PHYS_2 = AGG_PHYS, 
+           MENT_2 = AGG_MENT, 
+           PHYS_2 = AGG_PHYS, 
            ADH_2 = ADH) %>%
     mutate(LOG_VLOAD_2 = log10(VLOAD_2),
            .keep = "unused")
 
-# Combine data, create outcome difference columns
+# Combine data, create outcome difference columns, create race and education
+#   covariates
 fullData <- full_join(yearZero, yearTwo, by = "NEWID") %>%
     mutate(LOG_VLOAD_DIFF = LOG_VLOAD_2 - LOG_VLOAD_0,
            LEU3N_DIFF = LEU3N_2 - LEU3N_0,
-           MENT_DIFF = AGG_MENT_2 - AGG_MENT_0,
-           PHYS_DIFF = AGG_PHYS_2 - AGG_PHYS_0) %>%
-    select(-LOG_VLOAD_2, -LEU3N_2, -AGG_MENT_2, -AGG_PHYS_2)
+           MENT_DIFF = MENT_2 - MENT_0,
+           PHYS_DIFF = PHYS_2 - PHYS_0) %>%
+    mutate(WHITE_NH = as.numeric(.data$RACE_0 == 1),
+           BLACK_NH = as.numeric(.data$RACE_0 == 3),
+           HISPANIC = as.numeric(.data$RACE_0 == 2 | .data$RACE_0 == 4 | 
+                .data$RACE_0 == 8),
+           OTHER = as.numeric(.data$RACE_0 > 4 & .data$RACE_0 < 8)) %>%
+    mutate(LESS_HS = as.numeric(.data$EDUC_0 <= 2),
+           HS = as.numeric(.data$EDUC_0 == 3 | .data$EDUC_0 == 4),
+           GREAT_HS = as.numeric(.data$EDUC_0 > 4)) %>%
+    mutate(SMOKE = as.numeric(.data$SMOKE_0 == 3)) %>%
+    select(-LOG_VLOAD_2, -LEU3N_2, -MENT_2, -PHYS_2,
+           -EDUC_0, -RACE_0, -NEWID, -SMOKE_0)
 
 # Keep only complete cases
 cleanData <- fullData %>% drop_na()
+
+# Create outcome specific data
+vloadData <- cleanData %>% 
+    select(-LEU3N_0, -LEU3N_DIFF,
+           -PHYS_0, -PHYS_DIFF,
+           -MENT_0, -MENT_DIFF)
+
+leu3nData <- cleanData %>% 
+    select(-LOG_VLOAD_0, -LOG_VLOAD_DIFF,
+           -PHYS_0, -PHYS_DIFF,
+           -MENT_0, -MENT_DIFF)
+
+mentData <- cleanData %>% 
+    select(-LOG_VLOAD_0, -LOG_VLOAD_DIFF, 
+           -LEU3N_0, -LEU3N_DIFF,
+           -PHYS_0, -PHYS_DIFF)
+
+physData <- cleanData %>% 
+    select(-LOG_VLOAD_0, -LOG_VLOAD_DIFF, 
+           -LEU3N_0, -LEU3N_DIFF,
+           -MENT_0, -MENT_DIFF)
