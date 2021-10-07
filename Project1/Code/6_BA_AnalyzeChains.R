37ee63011f9cc79102fcd266f3cf98385a074e87
Max McGrath <max.a.mcgrath@gmail.com>
2021-10-06 19:01:26 -0600

    Add code to collect deviances

--- Project1/Code/6_BA_AnalyzeChains.R
+++ Project1/Code/6_BA_AnalyzeChains.R
@@ -1,5 +1,6 @@
 source("Code/4_BA_DrawChains.R")
 
+# Means and credible intervals -------------------------------------------------
 # Summarize VLOAD chains
 vloadChains <- bind_rows(
     "uniNoDrugs" = pivot_longer(bind_rows(as.data.frame(vloadUniNoDrugsChains[[1]]),
@@ -107,3 +108,43 @@ physChainsSummary <- physChains %>%
     summarize(mean = mean(value),
               upper95 = quantile(value, .95),
               lower95 = quantile(value, .05))
+
+
+# DIC --------------------------------------------------------------------------
+deviances <- data.frame("model" = c("vloadUniNoDrugs", "vloadUni", "vloadMultiNoDrugs", "vloadMulti", 
+                                    "leu3nUniNoDrugs", "leu3nUni", "leu3nMultiNoDrugs", "leu3nMulti",
+                                    "mentUniNoDrugs", "mentUni", "mentMultiNoDrugs", "mentMulti",
+                                    "physUniNoDrugs", "physUni", "physMultiNoDrugs", "physMulti"),
+                        "penDIC" = c(sum(vloadUniNoDrugsDIC$deviance), 
+                                     sum(vloadUniDIC$deviance),
+                                     sum(vloadMultiNoDrugsDIC$deviance), 
+                                     sum(vloadMultiDIC$deviance),
+                                     sum(leu3nUniNoDrugsDIC$deviance), 
+                                     sum(leu3nUniDIC$deviance),
+                                     sum(leu3nMultiNoDrugsDIC$deviance),
+                                     sum(leu3nMultiDIC$deviance), 
+                                     sum(mentUniNoDrugsDIC$deviance),
+                                     sum(mentUniDIC$deviance), 
+                                     sum(mentMultiNoDrugsDIC$deviance),
+                                     sum(mentMultiDIC$deviance), 
+                                     sum(physUniNoDrugsDIC$deviance), 
+                                     sum(physUniDIC$deviance),
+                                     sum(physMultiNoDrugsDIC$deviance), 
+                                     sum(physMultiDIC$deviance)),
+                        "DIC" = c(sum(vloadUniNoDrugsDIC$deviance) + sum(vloadUniNoDrugsDIC[[2]]),
+                                  sum(vloadUniDIC$deviance) + sum(vloadUniDIC[[2]]),
+                                  sum(vloadMultiNoDrugsDIC$deviance) + sum(vloadMultiNoDrugsDIC[[2]]),
+                                  sum(vloadMultiDIC$deviance) + sum(vloadMultiDIC[[2]]),
+                                  sum(leu3nUniNoDrugsDIC$deviance) + sum(leu3nUniNoDrugsDIC[[2]]),
+                                  sum(leu3nUniDIC$deviance) + sum(leu3nUniDIC[[2]]),
+                                  sum(leu3nMultiNoDrugsDIC$deviance) + sum(leu3nMultiNoDrugsDIC[[2]]),
+                                  sum(leu3nMultiDIC$deviance) + sum(leu3nMultiDIC[[2]]),
+                                  sum(mentUniNoDrugsDIC$deviance) + sum(mentUniNoDrugsDIC[[2]]),
+                                  sum(mentUniDIC$deviance) + sum(mentUniDIC[[2]]),
+                                  sum(mentMultiNoDrugsDIC$deviance) + sum(mentMultiNoDrugsDIC[[2]]),
+                                  sum(mentMultiDIC$deviance) + sum(mentMultiDIC[[2]]),
+                                  sum(physUniNoDrugsDIC$deviance) + sum(physUniNoDrugsDIC[[2]]),
+                                  sum(physUniDIC$deviance) + sum(physUniDIC[[2]]),
+                                  sum(physMultiNoDrugsDIC$deviance) + sum(physMultiNoDrugsDIC[[2]]),
+                                  sum(physMultiDIC$deviance) + sum(physMultiDIC[[2]]))
+                        )
