c5e9169aeec771ca33a0c4d0705569d5c1ac0da8
Max McGrath <max.a.mcgrath@gmail.com>
2021-11-09 13:11:22 -0700

    Update data processing, add to interim report

--- Project3/Code/1_ProcessData.R
+++ Project3/Code/1_ProcessData.R
@@ -7,11 +7,19 @@ rawData <- read.csv("DataRaw/frmgham2.csv")
 # Filter individuals who previously had stroke, create censorship/event
 #   indicator and event time column. 
 cleanData <- rawData %>%
-    filter(!(.data$PERIOD == 1 & .data$PREVSTRK == 1)) %>%
-    mutate(timestroke10yr = pmin(.data$TIMESTRK, .data$TIMEDTH, 3650), 
+    mutate(timestroke10yr = pmin(.data$TIMESTRK, 3650), 
            stroke10yr = ifelse(timestroke10yr < 3650, 1, 0)) %>%
+    filter(.data$PERIOD == 1) %>%
+    filter(!(.data$PREVSTRK == 1)) %>%
     # Reorder columns, only keep those which are needed
-    select(RANDID, SEX, TIME, PERIOD, TIMESTRK, stroke10yr, timestroke10yr) %>% 
-    filter(.data$PERIOD == 1)
+    select(RANDID, SEX, TIME, PERIOD, TIMESTRK, stroke10yr, timestroke10yr)
 
-
+# Check sample size values by M/F and event/censored
+nEventM <- length(cleanData$stroke10yr[cleanData$SEX == 1 & 
+                                           cleanData$stroke10yr == 1])
+nEventF <- length(cleanData$stroke10yr[cleanData$SEX == 2 & 
+                                           cleanData$stroke10yr == 1])
+nCensoredM <- length(cleanData$stroke10yr[cleanData$SEX == 1 & 
+                                           cleanData$stroke10yr == 0])
+nCensoredF <- length(cleanData$stroke10yr[cleanData$SEX == 2 & 
+                                           cleanData$stroke10yr == 0])
